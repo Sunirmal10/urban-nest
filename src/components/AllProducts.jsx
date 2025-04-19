a9212d8916d78e2ea0ef175c9b5a0cd469fbd23b
SUNIRMAL DAS <sunirmald1996@gmail.com>
2025-04-20 05:07:14 +0530

    updated responsiveness

--- src/components/AllProducts.jsx
+++ src/components/AllProducts.jsx
@@ -6,9 +6,12 @@ import { Link } from 'react-router-dom'
 const AllProducts = () => {
   const [loadMore, setLoadMore] = useState(12)
   return (
-    <div className='flex flex-wrap justify-center gap-2 md:gap-[20px] md:px-6 px-1 pt-10 md:pt-14 items-center relative'>
+    <main className='flex flex-col gap-1'>
+         <p className='flex items-center md:gap-4 gap-2 font-semibold pt-2 md:pt-5 md:pl-6 pl-2'>ALL PRODUCTS <span className='text-xs text-slate-400 font-normal hover:font-medium hover:underline hover:text-slate-700'><Link to={'/product-list'}>view all</Link></span></p>
 
-              <p className='flex items-center md:gap-4 gap-2 font-semibold absolute top-2 md:top-5 md:left-6 left-2'>ALL PRODUCTS <span className='text-xs text-slate-400 font-normal hover:font-medium hover:underline hover:text-slate-700'><Link to={'/product-list'}>view all</Link></span></p>
+         <div className='flex flex-wrap justify-center gap-2 md:gap-[20px] md:px-6 px-1 pt-1 items-center'>
+
+           
       {
         products.slice(0, loadMore).map((item)=>(
          
@@ -16,6 +19,10 @@ const AllProducts = () => {
             
         ))
       }
+     
+      </div>
+      {/* Buttons  */}
+      <div className='flex mt-3 justify-center items-center'>
       {
         loadMore < 36 ?
       
@@ -32,6 +39,9 @@ const AllProducts = () => {
       </Link>
     }
       </div>
+     
+    </main>
+    
   )
 }
 
